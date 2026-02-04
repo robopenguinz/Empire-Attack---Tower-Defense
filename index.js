0e19470a351328888d9baaeb51f3bd36c51ffebd
robopenguinz <chrism900526@gmail.com>
2026-02-04 14:42:33 -0800

    Created Enemy Class

--- index.js
+++ index.js
@@ -8,7 +8,50 @@ c.fillStyle = 'white'
 c.fillRect(0, 0, canvas.width, canvas.height)
 
 const image = new Image()
+
 image.onload = () => {
-    c.drawImage(image, 0, 0)
+    animate()
 }
 image.src = 'img/map1.png'
+
+class Ememy {
+    constructor() {
+    this.position = {
+        x: 0,
+        y: 0
+    }
+    this.width = 100
+    this.height = 100
+    }
+
+draw() {
+    c.fillStyle = 'red'
+    c.fillRect(this.position.x, this.position.y, this.width, this.height)
+}
+
+update() {
+    this.draw()
+    this.position.x += 1
+    this.position.y += 1
+}
+}
+
+const ememy = new Ememy()
+
+function animate() {
+    requestAnimationFrame(animate)
+
+    c.drawImage(image, 0, 0)
+    ememy.update()
+}
+
+let x = 200
+function animate() {
+    requestAnimationFrame(animate)
+
+    c.drawImage(image, 0, 0)
+
+    c.fillStyle = 'red'
+    c.fillRect(200, 400, 100, 100)
+    x++
+}
